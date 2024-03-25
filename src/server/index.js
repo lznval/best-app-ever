d6c93436eb878598b8768d5916f830c2394964f8
Александр Лизунов <a.lizunov@studyworld.io>
2024-03-25 16:54:02 +0400

    Добавили подключение к БД

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
-app.listen(5000, () => {
-  console.log("Server started!");
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "All is good!" });
 });
+
+async function start() {
+  try {
+    await mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.bcqajau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    app.listen(PORT, () => console.log(`Server started on: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
