95c762c7b75c16226be4c0be22e4ebe84b048104
Александр Лизунов <a.lizunov@studyworld.io>
2024-05-09 12:48:30 +0400

    Доработка работы с MongoDB

--- src/server/index.js
+++ src/server/index.js
@@ -2,29 +2,64 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
+import bcrypt from "bcryptjs";
 
-const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const USER = process.env.DB_USER;
 const PASSWORD = process.env.DB_PASSWORD;
+mongoose
+  .connect(
+    `mongodb+srv://${USER}:${PASSWORD}@cluster0.bcqajau.mongodb.net/market?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => console.log("DB OK"))
+  .catch(err => console.log("DB error", err));
 
-// Middleware
-app.use(cors());
+const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "All is good!" });
+  res.send("Hello world!");
 });
 
-async function start() {
+app.post("/auth/register", registerValidation, async (req, res) => {
   try {
-    await mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.bcqajau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
-    app.listen(PORT, () => console.log(`Server started on: ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
-}
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-start();
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      passwordHash,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
+  }
+});
+
+app.listen(PORT, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server OK");
+});
