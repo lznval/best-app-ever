fc8d454f6c816cba71e7092d3bb72a0750175da9
Александр Лизунов <a.lizunov@studyworld.io>
2024-06-25 16:08:32 +0400

    Пофиксил немного Каталог, перенёс всё в один компонент

--- src/client/components/Header/index.tsx
+++ src/client/components/Header/index.tsx
@@ -1,30 +1,18 @@
 import CatalogMenu from '@components/UI/CatalogMenu';
 import styles from './Header.module.scss';
-import { FC, useRef, useState } from 'react';
+import { FC } from 'react';
 import { HeartIcon, OrdersIcon, CartIcon, UserIcon } from '@components/Icons';
 import { Link } from 'react-router-dom';
 import { Input } from '@components/UI/Input';
-import { useClickOutside } from 'client/utils/hooks';
 
-export const Header: FC = () => {
-
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const catalogRef = useRef<HTMLDivElement>(null);
-
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
-
-  useClickOutside(catalogRef, handleClick);
-  
+export const Header: FC = () => {  
   return (
     <header className={styles.wrapper}>
       <Link to='/' className={styles.logo}>
         PoopMarket
       </Link>
-      <div ref={catalogRef} className={styles.catalog} onClick={handleClick}>
-        <CatalogMenu isOpen={isOpen} />
-        Каталог
+      <div className={styles.catalog}>
+        <CatalogMenu />
       </div>
       <div className={styles.search}>
         <Input placeholder='Поиск' />
--- src/client/components/UI/CatalogMenu/index.tsx
+++ src/client/components/UI/CatalogMenu/index.tsx
@@ -1,19 +1,27 @@
-import React, { FC, RefObject, useState } from 'react';
+import React, { FC, RefObject, useRef, useState } from 'react';
 import styles from './CatalogMenu.module.scss';
 import cn from 'classnames';
+import { useClickOutside } from 'client/utils/hooks';
 
-type ICatalogMenuProps = {
-  isOpen: boolean,
-}
+const CatalogMenu: FC = ( ) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const catalogRef = useRef<HTMLDivElement>(null);
 
-const CatalogMenu: FC<ICatalogMenuProps> = ({ isOpen }) => {
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useClickOutside(catalogRef, handleClick);
   return (
-    <div
-      className={cn(styles.menu, { [styles.open]: isOpen })}
-    >
-      <span></span>
-      <span></span>
-      <span></span>
+    <div className={cn(styles.catalog, { [styles.open]: isOpen } )} ref={catalogRef} onClick={handleClick}>
+      <div
+        className={styles.menu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <p>Каталог</p>
     </div>
   );
 };
